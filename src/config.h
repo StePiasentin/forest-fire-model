cc31062ec66bf743f15968078e90a1879ec6d94a
stepx0 <piasentin.st@gmail.com>
2025-08-01 18:08:30 +0200

    all files formatted / TREE and EMPTY checks added, FIRE in todo

--- src/config.h
+++ src/config.h
@@ -10,8 +10,14 @@
 #define TREE 1
 #define FIRE 2
 
-#define TREE_T0_SPAWN_RATE 0.6  // spawn tree probability at T0 time
-#define TREE_SPAWN_RATE 0.01  // new tree growth probability
+#define TREE_T0_SPAWN_RATE 0.6   // spawn tree probability at T0 time
+#define TREE_SPAWN_RATE 0.01     // new tree growth probability
 #define FIRE_IGNITION_RATE 0.001 // new fire starting probability
+
+// converts from 2D to 1D array coordinates
+#define COORDINATES_TO_INDEX(x, y) ((y) * WIDTH + (x))
+
+// checks if forest margins are respected
+#define FOREST_MARGINS_CHECK(x, y) ((x) >= 0 && (x) < WIDTH && (y >= 0) && (y) <= HEIGHT)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,21 +1,23 @@
 #include "forest.h"
 
-int main() {
+int main()
+{
     int forest1[WIDTH * HEIGHT]; // forest at odd T time instant
     int forest2[WIDTH * HEIGHT]; // forest status at event T time instant
-    int* current = forest1;
-    int* next = forest2;
+    int *current = forest1;
+    int *next = forest2;
 
     init_grid(current);
 
-    //WIP ...
-    
-    while (1) {
+    // WIP ...
+
+    while (1)
+    {
         render_grid(current);
         update_grid(current, next);
 
         // grids swapping logic
-        int* temp = current;
+        int *temp = current;
         current = next;
         next = temp;
     }
