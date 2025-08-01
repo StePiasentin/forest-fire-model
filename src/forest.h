af070e810e1ece79d9d4a86ab7252406073af85a
stepx0 <piasentin.st@gmail.com>
2025-08-01 17:06:37 +0200

    files macro structure defined

---
+++ src/forest.h
@@ -0,0 +1,10 @@
+#ifndef FOREST_H
+#define FOREST_H
+
+#include "config.h"
+
+void init_grid(int* grid);
+void update_grid(const int* current, int* next);
+void render_grid(const int* grid);
+
+#endif
