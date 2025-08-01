a03523bf1a6d3d47555df4b058931f70a031cf83
piasentinstefano <stefano.piasentin@dsgroup.it>
2025-08-01 19:26:26 +0200

    fire logics added / fixed some issues in calcs

--- src/forest.c
+++ src/forest.c
@@ -24,18 +24,54 @@ void update_grid(const int *current, int *next)
         switch (current[i])
         {
         case FIRE:
-            // todo: ...
+            // fire ends in T+1
+            next[i] = EMPTY;
             break;
 
         case TREE:
+            // casual ignition probability
             next[i] = ((float)rand() / RAND_MAX < FIRE_IGNITION_RATE) ? FIRE : TREE;
+            if (next[i] == FIRE)
+                break;
+
+            // checks adjacent fires
+            if(is_neighbor_on_fire(current, i, GET_X(i) - 1, GET_Y(i) - 1)) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i)    , GET_Y(i) - 1)) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i) + 1, GET_Y(i) - 1)) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i) - 1, GET_Y(i)    )) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i) + 1, GET_Y(i)    )) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i) - 1, GET_Y(i) + 1)) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i)    , GET_Y(i) + 1)) { next[i] = FIRE; break; }
+            if(is_neighbor_on_fire(current, i, GET_X(i) + 1, GET_Y(i) + 1)) { next[i] = FIRE; break; }  
             break;
 
         case EMPTY:
+            // can spawn a tree
             next[i] = ((float)rand() / RAND_MAX < TREE_SPAWN_RATE) ? TREE : EMPTY;
             break;
         }
     }
+}
+
+// checks if neighbor tree is burning
+int is_neighbor_on_fire(const int *current, int current_i, int x_to_check, int y_to_check)
+{
+    if (!FOREST_MARGIN_CHECK(x_to_check, y_to_check))
+        return 0;
+
+    // blocking horizontal wrap-around:
+    // conditions:
+    // x ∈ [cx - 1, cx + 1]
+    // y ∈ [cy - 1, cy + 1]
+
+    if (x_to_check < GET_X(current_i) - 1 ||
+        x_to_check > GET_X(current_i) + 1 ||
+        y_to_check < GET_Y(current_i) - 1 ||
+        y_to_check > GET_Y(current_i) + 1)
+        return 0;
+
+    int index = COORDINATES_TO_INDEX(x_to_check, y_to_check);
+    return current[index] == FIRE;
 }
 
 // grid rendering logic
