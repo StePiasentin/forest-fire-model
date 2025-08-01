97a3c091b0c06dd2f46729bb317909ecfeb40fea
piasentinstefano <stefano.piasentin@dsgroup.it>
2025-08-01 18:08:30 +0200

    all files formatted / TREE and EMPTY checks added, FIRE in todo

--- src/forest.c
+++ src/forest.c
@@ -5,17 +5,41 @@
 #include "config.h"
 
 // starts the forest with a causal pattern
-void init_grid(int* grid) {
-    for(int i = 0; i < WIDTH * HEIGHT; i ++) {
+void init_grid(int *grid)
+{
+    for (int i = 0; i < WIDTH * HEIGHT; i++)
+    {
         grid[i] = ((float)rand() / RAND_MAX < TREE_T0_SPAWN_RATE) ? TREE : EMPTY;
     }
 }
 
 // updates grid state
-void update_grid(const int* current, int* next);
+void update_grid(const int *current, int *next)
+{
+    //(x-1,y-1) (x, y-1) (x+1,y-1)
+    //(x-1,y)   (x, y)   (x+1,y)
+    //(x-1,y+1) (x, y+1) (x+1,y+1)
+    for (int i = 0; i < WIDTH * HEIGHT; i++)
+    {
+        switch (current[i])
+        {
+        case FIRE:
+            // todo: ...
+            break;
+
+        case TREE:
+            next[i] = ((float)rand() / RAND_MAX < FIRE_IGNITION_RATE) ? FIRE : TREE;
+            break;
+
+        case EMPTY:
+            next[i] = ((float)rand() / RAND_MAX < TREE_SPAWN_RATE) ? TREE : EMPTY;
+            break;
+        }
+    }
+}
 
 // grid rendering logic
-void render_grid(const int* grid);
+void render_grid(const int *grid);
 
 // frame delay (needed?)
 void post_frame_delay(void);
--- src/forest.h
+++ src/forest.h
@@ -3,8 +3,8 @@
 
 #include "config.h"
 
-void init_grid(int* grid);
-void update_grid(const int* current, int* next);
-void render_grid(const int* grid);
+void init_grid(int *grid);
+void update_grid(const int *current, int *next);
+void render_grid(const int *grid);
 
 #endif
